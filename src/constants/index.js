b4d09ed2aa4f7386f5dc213883506d7541ce1c66
dilmurod khamidov <dilmuroddelitot@gmail.com>
2023-04-26 23:32:06 +0500

    constants category and color

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const category = [
     {name: "Live", icon: <LiveTv/>},
     {name: "Gaming", icon: <SportsEsports/>},
     {name: "Education", icon: <School/>},
-    {name: "Sport", icon: <FitnessCenter/>},
+    {name: "Sport", icon: <FitnessCenter/>}, 
     {name: "Comedy", icon: <TheaterComedy/>},
     {name: "Podcast", icon: <GraphicEq/>},
     {name: "Fashion", icon: <Checkroom/>},
